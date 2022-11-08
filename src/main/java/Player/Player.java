62c0282bfb1159788c23a7674e0f025110f58621
Mateusz Kuliński <kula.mat91@gmail.com>
2022-11-08 23:39:17 +0100

    Ship commit 1

---
+++ src/main/java/Player/Player.java
@@ -0,0 +1,10 @@
+package Player;
+/*
+    Class Player has logic responsible for handling shot.
+    Class Player has field of type List<ShipType>.
+    Class Player has a method isAlive to check if player has not lost all ships and returns true or false accordingly.
+ */
+
+public class Player {
+
+}
---
+++ src/main/java/Ship/ShipType.java
@@ -0,0 +1,37 @@
+package Ship;
+/*
+Implement enum ShipType and it's methods.
+
+    Enum ShipType represents ship types -
+    Each ShipType has a unique length.
+    
+    Do dyspozycji gracze mają najczęściej:
+
+    Cztery jednomasztowce (czyli statki na jedną kratkę),
+    Trzy dwumasztowce (statki na dwie kratki),
+    Dwa trzymasztowce (statki na trzy kratki),
+    Oraz jeden czteromasztowiec (statek na cztery kratki).
+ */
+
+public enum ShipType {
+    BOAT(1, 4),
+    SAIL_BOAT(2,3),
+    SUBMARINE(3,2),
+    BLACK_PEARL(4, 1),
+    ;
+
+    public final int size;
+    public final int number;
+
+    ShipType(int size, int number) {
+        this.size = size;
+        this.number = number;
+    }
+
+    public int getSize(){
+        return size;
+    }
+    public int getNumber(){
+        return number;
+    }
+}
